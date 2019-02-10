498d5b41a6ac545f56ae869942043b240a4e7f68
Anirudh <av.anirudh.96@gmail.com>
2019-02-10 10:24:57 -0500

    Added RockPaperScissors game

---
+++ Stage 3/Lesson2/rps.py
@@ -0,0 +1,31 @@
+print("Welcome! Let's play Rock! Paper! Scissors!\n")
+print("I am playing....., what will you choose?\n")
+user=input("\nRock\nPaper\nScissors\n\n")
+if(user=="Rock"):
+    print("The Rocks smashed into each other!Its a tie. Ok let me play something else\n")
+elif(user=="Paper"):
+    print("Good play! I'm defeated. Lets do two more rounds\n")
+else:
+    print("I broke your scissors with my rock, Bad Luck! Let's continue\n")
+
+
+    
+print("I am playing....., what will you choose?\n")
+user=input("\nRock\nPaper\nScissors\n")
+if(user=="Rock"):
+    print("My Paper beats Rock, better luck next time! Time for the last round\n")
+elif(user=="Paper"):
+    print("Let's just move on.\n")
+else:
+    print("Oh no! my Paper's shredded.You Win! Let's see who wins next.\n")
+
+
+    
+print("I am playing....., what will you choose?\n")
+user=input("\n1. Rock\n2. Paper\n3. Scissors\n")
+if(user=="Rock"):
+    print("My scissors are smashed! Congrats, you win! Thanks for playing hope you enjoyed it!\n")
+elif(user=="Paper"):
+    print("Yay, I chose scissors! I win. Lets meet next time!\n")
+else:
+    print("Looks like we can play Fencing now!Goodbye!\n")
